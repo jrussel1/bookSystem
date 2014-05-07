ec4a99d1cf5c200fe6784bebbdf1e9f4f8e590c0
Phillip Stuhr <pstuhr@macalester.edu>
2014-05-06 23:08:27 -0500

    Created Splash Page
    Created a splash page, just used an image of a book that we already had
    because I wasn't sure what image we wanted to use.  All we need to do to
    change it is make a new drawable.

--- book.system.android.appengine/src/com/book/system/android/appengine/MainActivity.java
+++ book.system.android.appengine/src/com/book/system/android/appengine/MainActivity.java
@@ -2,26 +2,44 @@ package com.book.system.android.appengine;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuInflater;
 
 public class MainActivity extends Activity {
 
+    private final int SPLASH_DISPLAY_LENGTH = 5000;
+	
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.splash);
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-
-		
+        /* New Handler to start the Menu-Activity 
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(MainActivity.this, BookListActivity.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+//	protected void onCreate(Bundle savedInstanceState) {
+//		super.onCreate(savedInstanceState);
+//		setContentView(R.layout.activity_main);
+//		
+//
+//		
+////		Intent intent = new Intent(MainActivity.this, BookListActivity.class);
 //		Intent intent = new Intent(MainActivity.this, BookListActivity.class);
-		Intent intent = new Intent(MainActivity.this, BookListActivity.class);
-		
-		
-
-		startActivity(intent);
-	}
+//		
+//		
+//
+//		startActivity(intent);
+//	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
