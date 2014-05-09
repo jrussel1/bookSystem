26d5bb3312a05e1d66c89850ae07a7553d778b41
jesse <jrussel1@macalester.edu>
2014-05-09 15:01:21 -0500

    sign in button
    We still need to add something else to the login page

--- book.system.android.appengine/src/com/book/system/android/appengine/MainActivity.java
+++ book.system.android.appengine/src/com/book/system/android/appengine/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.MenuInflater;
 
 public class MainActivity extends Activity {
 
-    private final int SPLASH_DISPLAY_LENGTH = 5000;
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
 	
     @Override
     public void onCreate(Bundle icicle) {
